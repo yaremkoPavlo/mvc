2b0dc0320a20d0054a47c8039fcaba1c7b893784
yaremkoPavlo <yaremko.pavlo@gmail.com>
2018-04-12 10:11:22 +0300

    add files

---
+++ app/controllers/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\controllers;
+
+
+abstract class Controller
+{
+
+}
---
+++ app/run.php
@@ -0,0 +1,11 @@
+<?php
+namespace app;
+
+function autoLoader ($class) {
+    $class = str_replace('\\','/',$class) . '.php';
+    if (file_exists($class)) {
+        require_once $class;
+    }
+}
+
+spl_autoload_register('autoLoader');
